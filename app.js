ee059f8ea20fe027ff0511b9e3ddd1eeb69896bf
Veronica Alcala <veronica.alcala@gmail.com>
2021-02-04 15:44:40 +0100

    iteration 1 and 2, done

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ require('./configs/debugger.config')
 // App
 const express = require('express')
 const app = express()
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+  
+  // Retrieve an access token
+  spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // Configs
 require('./configs/preformatter.config')(app)
