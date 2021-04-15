d30f5ec0a91a304ddac7bce6b4a2d1237e7aeaa5
Mario Ortiz <ortiz.mario22@gmail.com>
2021-04-15 18:52:15 +0200

    too long exercise

--- app.js
+++ app.js
@@ -9,6 +9,21 @@ require('./configs/debugger.config')
 // App
 const express = require('express')
 const app = express()
+const SpotifyWebApi = require('spotify-web-api-node');
+
+//Spotify
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+});
+// Retrieve an access token
+spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .then()
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
+
 
 // Configs
 require('./configs/preformatter.config')(app)
@@ -17,5 +32,20 @@ require('./configs/views.configs')(app)
 require('./configs/locals.config')(app)
 // Routes index
 require('./routes')(app)
+
+//Routes for the lab:
+app.get('/artist-search?artist=:artist', (req, res, next) => {
+    spotifyApi.searchArtists({ 'artist': req.params.artist })
+    console.log(req.params);
+    // spotifyApi
+    //     .searchArtists('Metallica')
+    //     .then(data => {
+    //         console.log('The received data from the API: ', data.body);
+    //         // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+    //     })
+    //     .catch(err => console.log('The error while searching artists occurred: ', err));
+
+}
+)
 
 module.exports = app
--- routes/base.routes.js
+++ routes/base.routes.js
@@ -1,8 +1,47 @@
 const express = require('express')
 const router = express.Router()
+const SpotifyWebApi = require('spotify-web-api-node');
+
+//Spotify
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+});
+// Retrieve an access token
+spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .then()
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
+
 
 // Endpoints
 router.get('/', (req, res) => res.render('index'))
+
+//Routes for the lab:
+router.get('/artist-search', (req, res, next) => {
+    spotifyApi
+        .searchArtists(req.query.artist)
+        .then(data => {
+            // console.log('The received data from the API: ', data.body.artists.items[0].images[0].url);
+            // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+            res.render('artist-search-results', { artists: data.body.artists.items })
+
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+})
+
+router.get('/albums/:id', (req, res) => {
+
+    const { id } = req.params
+    spotifyApi
+        .getArtistAlbums(id)
+        .then(data => {
+            console.log(data.body);
+        })
+})
+
 
 
 module.exports = router
