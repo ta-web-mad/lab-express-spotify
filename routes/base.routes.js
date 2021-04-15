0022d3eaa8cf148b42f77897b3804cae57592eee
Anthony Guido <guido.anthony@orange.fr>
2021-04-15 18:32:01 +0200

    Spotify exercise done until Iteration 3

--- routes/base.routes.js
+++ routes/base.routes.js
@@ -1,8 +1,35 @@
 const express = require('express')
+const app = require('../app')
 const router = express.Router()
+
+const SpotifyWebApi = require('spotify-web-api-node')
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
+
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+  .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // Endpoints
 router.get('/', (req, res) => res.render('index'))
+// router.get('/artist-results', (req, res) => )
+
+// Query strings
+router.get('/artist-results', (req, res) => {
+    console.log(req.query.artist)
+    spotifyApi
+    .searchArtists(req.query.artist)
+    .then(data => {
+    console.log('The received data from the API: ', data.body)
+    res.render('artist-search-results', data.body)
+    })
+    .catch(err => console.log('The error while searching artists occurred: ', err))
+    // res.render('artist-results', req.query)
+})
 
 
 module.exports = router
