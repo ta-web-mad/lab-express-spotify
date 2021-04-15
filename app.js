12604b2dc75bd837ef17f0455ce88a2cba488edb
Jaime Bastos-mendes armijo <jaimebastos-mendesarmijo@MacBook-Air-de-Jaime.local>
2021-04-15 18:31:02 +0200

    done

--- app.js
+++ app.js
@@ -9,6 +9,28 @@ require('./configs/debugger.config')
 // App
 const express = require('express')
 const app = express()
+const hbs = require('hbs')
+const SpotifyWebApi = require('spotify-web-api-node');
+
+
+
+// --------------0----------------
+
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
+
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+  .catch(error => console.log('Something went wrong when retrieving an access token', error));
+
+
+
+// --------------0----------------
+
 
 // Configs
 require('./configs/preformatter.config')(app)
