985a95b314dfc4534b12b812616775b5cbf83a59
Achraf <Achrafarazzouk@gmail.com>
2021-04-15 21:56:27 +0200

    done

--- app.js
+++ app.js
@@ -9,13 +9,19 @@ require('./configs/debugger.config')
 // App
 const express = require('express')
 const app = express()
+const SpotifyWebApi = require('spotify-web-api-node');
 
 // Configs
 require('./configs/preformatter.config')(app)
 require('./configs/middleware.config')(app)
 require('./configs/views.configs')(app)
 require('./configs/locals.config')(app)
+
 // Routes index
 require('./routes')(app)
+
+
+
+
 
 module.exports = app
